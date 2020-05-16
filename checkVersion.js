e68e93d507bfc7ffd293e4e8b1a0e379236f2671
tingzhouu <limtingzhou@gmail.com>
2020-05-16 14:06:16 +0800

    update check version for app store

--- checkVersion.js
+++ checkVersion.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
 
-const url = 'https://apps.apple.com/sg/app/policypal/id1118511972';
+const url = "https://apps.apple.com/sg/app/policypal/id1118511972";
 const TIME_INTERVAL_IN_MINUTES = 5;
-const CURRENT_APP_VERSION = 'Version 3.3.14';
+const CURRENT_APP_VERSION = "Version 3.3.20";
 
 async function checkIfAppVersionHasChanged() {
   const response = await axios.get(url);
   const data = response.data;
-  
+
   const isSameAppVersion = data.includes(CURRENT_APP_VERSION);
-  const timeNow = moment().format('h:mm:ss a');
-  const outputString = `Run at ${timeNow}, app version has ${isSameAppVersion ? 'not changed yet.' : 'HAS CHANGED!'}`;
+  const timeNow = moment().format("h:mm:ss a");
+  const outputString = `Run at ${timeNow}, app version has ${
+    isSameAppVersion ? "not changed yet." : "HAS CHANGED!"
+  }`;
   console.log(outputString);
 }
 
